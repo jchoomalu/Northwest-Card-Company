24abca9ead2e1e539c144651db8a607fb4d4f12a
Jason Hoomalu <jasonhoomalu@Jasons-Air.lan>
2023-11-06 11:35:34 -0700

    basic nav and sliding image complete

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,14 +5,32 @@ import {
   NavDropdown,
   FormControl,
   Form,
-  Container
+  Container,
+  Image,
 } from "react-bootstrap";
-import './styles.css'
+import {useState, useEffect} from 'react'
+import logo from "../../assets/logo2.png";
+import vetBadge from "../../assets/VeteranOwnedBadge.png";
+import accent from "../../assets/accentBar.png";
+import "./styles.css";
 
 const Navigation = () => {
+  const [slideIn, setSlideIn] = useState(false)
+  useEffect(() => {
+    // Use a delay to trigger the slide-in effect when the component is mounted
+    const timeout = setTimeout(() => {
+      setSlideIn(true);
+    }, 500); // Adjust the delay (500ms) as needed
+
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <Navbar className="p-2" collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <>
+    <Navbar variant="light" className="p-2" collapseOnSelect expand="lg">
+      <Navbar.Brand href="#home">
+        <Image className="logo" src={logo} />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
@@ -31,13 +49,22 @@ const Navigation = () => {
           </NavDropdown>
         </Nav>
         <Container className="searchContainer">
-        <Form className="d-flex">
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
+          <Form className="d-flex">
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2 outline-dark"
+            />
+            <Button variant="outline-light">Search</Button>
+          </Form>
         </Container>
+          <Image className="vetBadge" src={vetBadge} />
       </Navbar.Collapse>
     </Navbar>
+    <Container className={`image-container ${slideIn ? 'slide-in' : ''}`}>
+      <Image className="accentImage" src={accent} />
+    </Container>
+    </>
   );
 };
 
